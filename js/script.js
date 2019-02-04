f518d3483a6585545943f9ddcbfa3c3fc5797305
Mamunur Rashid <43251853+Mrmmamun@users.noreply.github.com>
2019-02-04 23:43:48 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+
+
+ function openNav() {
+  document.getElementById("mySidenav").style.width = "468px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+    
